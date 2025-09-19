0a027a4cdd3ccf1d7810c76a01c17f1ba8329e9a
Adam <adam.samec@gmail.com>
2025-09-19 18:04:24 +0200

    Add braille textbox with speech off and manage speech

--- addon/globalPlugins/brailleTeacher/brailleTeacher.py
+++ addon/globalPlugins/brailleTeacher/brailleTeacher.py
@@ -4,25 +4,36 @@
 import braille
 import gui
 import speech
+import ui
 import wx
 
 class BrailleTeacher:
+	def __init__(self):
+		self.prevSpeechMode = None
+
 	def show(self):
 		window = MainWindow(gui.mainFrame, self, title="Braille Teacher")
 		window.Raise()
 		window.Show()
 
+	def muteSpeech(self):
+		self.prevSpeechMode = speech.getState().speechMode
+		speech.setSpeechMode(speech.SpeechMode.off)
+
+	def restoreSpeech(self):
+		if self.prevSpeechMode is not None:
+			speech.setSpeechMode(self.prevSpeechMode)
+			self.prevSpeechMode = None
+
 	def displayBraille(self):
 		braille.handler.message("Hello world")
-
-	def muteSpeech(self):
-		speech.setSpeechMode(speech.SpeechMode.off)
 
 class MainWindow(wx.Frame):
 	def __init__(self, parent, app, title):
 		super(MainWindow, self).__init__(parent, title=title)
 
 		self.app = app
+		self.Bind(wx.EVT_CLOSE, self.handleWindowClose)
 
 		self.addWidgets()
 		self.SetSize((600, 400))
@@ -35,7 +46,10 @@ class MainWindow(wx.Frame):
 		self.showBrailleButton = sHelper.addItem(wx.Button(self.panel, label="Show test in braille"))
 		self.showBrailleButton.Bind(wx.EVT_BUTTON, self.handleShowBrailleButtonClick)
 
-		# self.brailleTextbox = sHelper.addLabeledControl("Read this braille", wx.TextCtrl, style=wx.TE_MULTILINE | wx.TE_READONLY)
+		self.brailleTextbox = sHelper.addLabeledControl("Read this braille", wx.TextCtrl, style=wx.TE_MULTILINE | wx.TE_READONLY | wx.TE_DONTWRAP)
+		self.brailleTextbox.Bind(wx.EVT_SET_FOCUS, self.handleBrailleTextboxFocus)
+		self.brailleTextbox.Bind(wx.EVT_KILL_FOCUS, self.handleBrailleTextboxFocusOut)
+		self.brailleTextbox.SetValue("Read this using your braille display. This may span several lines.")
 
 		self.transcriptTextbox = sHelper.addLabeledControl("Transcribe characters from braille", wx.TextCtrl, style=wx.TE_MULTILINE)
 
@@ -46,8 +60,19 @@ class MainWindow(wx.Frame):
 		mainSizer.Fit(self)
 		self.panel.SetSizer(mainSizer)
 
+	def handleWindowClose(self, event):
+		self.app.restoreSpeech()
+		self.Destroy()
+
 	def handleShowBrailleButtonClick(self, event):
 		self.app.displayBraille()
+
+	def handleBrailleTextboxFocus(self, event):
+		speech.speakMessage("Read braille")
+		self.app.muteSpeech()
+
+	def handleBrailleTextboxFocusOut(self, event):
+		self.app.restoreSpeech()
 
 	def handleVerifyButtonClick(self, event):
 		dialog = CorrectTranscriptDialog(self, title="Correct transcript")
